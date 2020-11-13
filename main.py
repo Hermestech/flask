ab15c566711ecd689a3f66b10ae87e9940800a93
Hermes <adanhermes23@gmail.com>
2020-11-13 13:34:46 -0600

    Segundo commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a productor']
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html', error=error)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+         'user_ip':user_ip,
+         'todos':todos,
+    }
+
+    return render_template('hello.html', **context)
